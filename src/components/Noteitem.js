c104f1f3a9269919bdc562824a4e7b449c5c67ae
Farmansk <shkfrmn786@gmail.com>
2022-06-13 10:24:09 +0530

    still bug

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -8,21 +8,25 @@ const Noteitem = (props) => {
     const { deleteNote } = context;
 
     // Delete Note
-    const deleteref = useRef(null);
+    const deleteref = useRef(0);
     const handleDelete = () => {
         deleteref.current.click();
     }
 
+    const handleConfirmDelete = () => {
+        deleteNote(note._id);
+    }
+
     return (
         <>
-            <button type="button" id="deletebtn" ref={deleteref} className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#staticBackdropdelete">
+            <button type="button" id="deletelaunchbtn" ref={deleteref} className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#staticBackdropdelete">
                 Launch static backdrop modal
             </button>
             <div className="modal fade" id="staticBackdropdelete" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="staticBackdropLabel">Delete Note</h5>
+                            <h5 className="modal-title" id="staticBackdropLabeldelete">Delete Note</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
@@ -30,7 +34,7 @@ const Noteitem = (props) => {
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">No</button>
-                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={() => { deleteNote(note._id) }}>Yes</button>
+                            <button type="button" className="btn btn-primary" id="deletebtn" data-bs-dismiss="modal" onClick={handleConfirmDelete}>Yes</button>
                         </div>
                     </div>
                 </div>
